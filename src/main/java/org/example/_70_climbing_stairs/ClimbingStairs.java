40adb882a77368d798d4140392ee924aeb2fd187
AlexeyPo <alexeypopov.dev@gmail.com>
2021-11-15 22:14:02 +0200

    added solution for problem #70 Climbing Stairs

---
+++ src/main/java/org/example/_70_climbing_stairs/ClimbingStairs.java
@@ -0,0 +1,19 @@
+package org.example._70_climbing_stairs;
+
+public class ClimbingStairs {
+
+    public int climbStairs(int n) {
+        int previous;
+        int current = 1;
+        int next = 1;
+
+        while (n > 0) {
+            previous = current;
+            current = next;
+            next = previous + current;
+            n--;
+        }
+
+        return current;
+    }
+}
---
+++ src/test/java/org/example/_70_climbing_stairs/ClimbingStairsTest.java
@@ -0,0 +1,38 @@
+package org.example._70_climbing_stairs;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClimbingStairsTest {
+
+    ClimbingStairs climbingStairs;
+
+    @BeforeEach
+    void init() {
+        climbingStairs = new ClimbingStairs();
+    }
+
+    @AfterEach
+    void destroy() {
+        climbingStairs = null;
+    }
+
+    @Test
+    void testShouldReturnTwoWays() {
+        int n = 2;
+        int expected = 2;
+        int actual = climbingStairs.climbStairs(n);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testShouldReturnThreeWays() {
+        int n = 3;
+        int expected = 3;
+        int actual = climbingStairs.climbStairs(n);
+        assertEquals(expected, actual);
+    }
+}
