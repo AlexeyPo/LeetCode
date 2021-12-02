4254a3f2210ca637dd1793a7361dc44cdb4b37ac
AlexeyPo <alexeypopov.dev@gmail.com>
2021-12-02 22:25:22 +0200

    solved SingleNumber

--- src/main/java/org/example/_136_single_number/SingleNumber.java
+++ src/main/java/org/example/_136_single_number/SingleNumber.java
@@ -1,22 +1,15 @@
 package org.example._136_single_number;
 
-import java.util.Arrays;
-
 public class SingleNumber {
 
     public int singleNumber(int[] nums) {
-        int num = 0;
         if (nums.length == 1) {
             return nums[0];
         }
-        Arrays.sort(nums);
-        int i;
-        for (i = 0; i < nums.length-1; i++) {
-            System.out.println(nums[i]);
-            System.out.println(nums[i+1]);
-            if (nums[i] == nums[i+1]) {
-                System.out.println("num = " + num);
-            }
+
+        int num = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            num = num ^ nums[i];
         }
         return num;
     }
