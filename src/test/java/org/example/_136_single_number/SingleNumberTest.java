9028cad888d23136707f8f60b78133624ae19857
AlexeyPo <alexeypopov.dev@gmail.com>
2021-12-02 18:05:22 +0200

    SingleNumber

---
+++ src/test/java/org/example/_136_single_number/SingleNumberTest.java
@@ -0,0 +1,47 @@
+package org.example._136_single_number;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SingleNumberTest {
+
+    SingleNumber singleNumber;
+
+    @BeforeEach
+    void init() {
+        singleNumber = new SingleNumber();
+    }
+
+    @AfterEach
+    void destroy() {
+        singleNumber = null;
+    }
+
+    @Test
+    void testSingleNumberWithThreeElementsArray() {
+        int[] nums = {2,2,1};
+        int expected = 1;
+        int actual = singleNumber.singleNumber(nums);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSingleNumberWithFiveElementsArray() {
+        int[] nums = {4,1,2,1,2};
+        int expected = 4;
+        int actual = singleNumber.singleNumber(nums);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSingleNumberWithOneElementArray() {
+        int[] nums = {3};
+        int expected = 3;
+        int actual = singleNumber.singleNumber(nums);
+        assertEquals(expected, actual);
+    }
+
+}
