b5b0f9b3a68a06b58bdb817d199d8f19053d8ba4
AlexeyPo <alexeypopov.dev@gmail.com>
2023-07-16 20:33:12 +0300

    added solution for #80 Remove Duplicates from Sorted Array II

---
+++ src/test/java/org/example/_80_remove_duplicates_from_sorted_array_second/RemoveDuplicatesFromSortedArraySecondTest.java
@@ -0,0 +1,32 @@
+package org.example._80_remove_duplicates_from_sorted_array_second;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveDuplicatesFromSortedArraySecondTest {
+
+    RemoveDuplicatesFromSortedArraySecond removeDuplicates;
+
+    @BeforeEach
+    void setUp() {
+        removeDuplicates = new RemoveDuplicatesFromSortedArraySecond();
+    }
+
+    @Test
+    void removeDuplicatesShouldReturnFive() {
+        int[] nums = {1, 1, 1, 2, 2, 3};
+        int expected = 5;
+        int actual = removeDuplicates.removeDuplicates(nums);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void removeDuplicatesShouldReturnSeven() {
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        int expected = 7;
+        int actual = removeDuplicates.removeDuplicates(nums);
+        assertEquals(expected, actual);
+    }
+}
