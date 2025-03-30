8820f36c9890cd86cb465a074f8747a83cade955
AlexeyPo <alexeypopov.dev@gmail.com>
2025-03-30 17:11:24 +0300

    added solution for #189 Rotate Array

---
+++ src/test/java/org/example/two_pointers/_189_rotate_array/RotateArrayTest.java
@@ -0,0 +1,40 @@
+package org.example.two_pointers._189_rotate_array;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RotateArrayTest {
+
+    RotateArray rotateArray;
+
+    @BeforeEach
+    void setUp() {
+        rotateArray = new RotateArray();
+    }
+
+    @AfterEach
+    void tearDown() {
+        rotateArray = null;
+    }
+
+    @Test
+    void rotateReturnResultAfterThreeSteps() {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+        int[] expected = {5, 6, 7, 1, 2, 3, 4};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+    }
+
+    @Test
+    void rotateReturnResultAfterTwoSteps() {
+        int[] nums = {-1,-100,3,99};
+        int k = 2;
+        int[] expected = {3,99,-1,-100};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+    }
+}
