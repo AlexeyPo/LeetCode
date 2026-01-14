f7874e9d2842f127978136e928d8b6b9ab3273ca
AlexeyPo <alexeypopov.dev@gmail.com>
2026-01-14 15:59:33 +0200

    added solution for #121 Best Time to Buy and Sell Stock

---
+++ src/main/java/org/example/_121_best_time_to_buy_and_sell_stock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+package org.example._121_best_time_to_buy_and_sell_stock;
+
+public class BestTimeToBuyAndSellStock {
+
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < min) {
+                min = price;
+            } else {
+                maxProfit = Math.max(maxProfit, price - min);
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ src/test/java/org/example/_121_best_time_to_buy_and_sell_stock/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,36 @@
+package org.example._121_best_time_to_buy_and_sell_stock;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BestTimeToBuyAndSellStockTest {
+
+    BestTimeToBuyAndSellStock stockSolver = new BestTimeToBuyAndSellStock();
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void maxProfitShouldReturnFive() {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int expectedProfit = 5;
+        int actualProfit = stockSolver.maxProfit(prices);
+        assertEquals(expectedProfit, actualProfit);
+    }
+
+    @Test
+    void maxProfitShouldReturnZero() {
+        int[] prices = {7, 6, 4, 3, 1};
+        int expectedProfit = 0;
+        int actualProfit = stockSolver.maxProfit(prices);
+        assertEquals(expectedProfit, actualProfit);
+    }
+}
